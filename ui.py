03c44fb2b23b5529f46171f51bb42453cc9a1e27
PietPadda <pietergdewaal@gmail.com>
2025-04-06 10:29:19 +0200

    converted all field types from local to instance vars using self. to call methods on them, then we added a method to link to dropdown to hide-show fields based on different duct types, need to add a .trace_add() method to dropdown field in order to call this new method AND call the method at end of our __init__, fixed bug in run_calculation where it was always considering rect fields or showing except error -- fixed by matching cases of duct types and just modifying to suit

--- ui.py
+++ ui.py
@@ -53,6 +53,9 @@ class UI:
         # after inputs, call the "terminal" output method
         self.create_terminal_output()
 
+        # once init is done, call toggle duct type fields
+        self.toggle_duct_type_fields()
+
     # WINDOW METHODS
     # need a method to update the visuals
     def redraw(self):
@@ -83,61 +86,101 @@ class UI:
         # DROPDOWN -- input by selection
         # DUCT TYPE SELECTOR
         # text label with white background
-        Label(input_frame, text="Duct Type", bg="white").grid(row=0, column=0, sticky="w", pady=5)
+        self.duct_types_label = Label(input_frame, text="Duct Type", bg="white")  # label instance
+        self.duct_types_label.grid(row=0, column=0, sticky="w", pady=5)  # grid position of label instance
         # first row/col, aligned to West, X px vertical pad
         # create tkinter var that holds dropdown selection
         self.duct_type_var = StringVar(value="Rectangular")  # defaults to Rect
         duct_types = ["Rectangular", "Round"]  # list of options
         # create dropdown menu
-        dropdown = OptionMenu(input_frame, self.duct_type_var, *duct_types)
+        self.dropdown = OptionMenu(input_frame, self.duct_type_var, *duct_types)
         # menu in frame, StringVar for updating, * = UNPACK the list as separate args
         # position the dropdown menu
-        dropdown.grid(row=0, column=1, sticky="w", pady=5)  # row1/col2, WEST, X px vertical pad
+        self.dropdown.grid(row=0, column=1, sticky="w", pady=5)  # row1/col2, WEST, X px vertical pad
+        # link toggle_duct_type_fields to the dropdown selector using trace
+        self.duct_type_var.trace_add("write", self.toggle_duct_type_fields)
+        # write = write operation watch, method to call on it
+
 
 
         # ENTRIES -- input typing text fields
-        # DUCT WIDTH
+        # DUCT WIDTH / DUCT DIAMETER (reuse same field for both entries)
         # text label with white background
-        Label(input_frame, text="Duct Width", bg="white").grid(row=1, column=0, sticky="w", pady=5)
+        self.width_label = Label(input_frame, text="Duct Width", bg="white")  # label instance
+        self.width_label.grid(row=1, column=0, sticky="w", pady=5)  # grid position of label instance
         self.width_var = StringVar()
         # creates a user entry widget for duct width
-        width_entry = Entry(input_frame, textvariable=self.width_var, width=7)  # auto updates when user types in field
+        self.width_entry = Entry(input_frame, textvariable=self.width_var, width=7)  # auto updates when user types in field
         # widht = x --> how wide the field is
         # positions entry widget
-        width_entry.grid(row=1, column=1, sticky="w", pady=5)
-        Label(input_frame, text="(mm)", bg="white").grid(row=1, column=2, sticky="e", pady=5)  # Unit label
+        self.width_entry.grid(row=1, column=1, sticky="w", pady=5)
+        self.width_unit = Label(input_frame, text="(mm)", bg="white")  # unit instance
+        self.width_unit.grid(row=1, column=2, sticky="e", pady=5)  # grid position of unit instance
 
         # DUCT HEIGHT
         # text label with white background
-        Label(input_frame, text="Duct Height", bg="white").grid(row=1, column=3, sticky="w", pady=5)
+        self.height_label = Label(input_frame, text="Duct Height", bg="white")  # label instance
+        self.height_label.grid(row=1, column=3, sticky="w", pady=5)  # grid position of label instance
         self.height_var = StringVar()
         # creates a user entry widget for duct width
-        width_entry = Entry(input_frame, textvariable=self.height_var, width=7)  # auto updates when user types in field
+        self.height_entry = Entry(input_frame, textvariable=self.height_var, width=7)  # auto updates when user types in field
         # positions entry widget
-        width_entry.grid(row=1, column=4, sticky="w", pady=5)
-        Label(input_frame, text="(mm)", bg="white").grid(row=1, column=5, sticky="e", pady=5)  # Unit label
+        self.height_entry.grid(row=1, column=4, sticky="w", pady=5)
+        self.height_unit = Label(input_frame, text="(mm)", bg="white")  # unit instance
+        self.height_unit.grid(row=1, column=5, sticky="e", pady=5)  # grid position of unit instance
 
         # FLOW RATE
         # text label with white background
-        Label(input_frame, text="Flow Rate", bg="white").grid(row=2, column=0, sticky="w", pady=5)
+        self.flow_rate_label = Label(input_frame, text="Flow Rate", bg="white")  # label instance
+        self.flow_rate_label.grid(row=2, column=0, sticky="w", pady=5)  # grid position of label instance
         self.flow_rate_var = StringVar()
         # creates a user entry widget for duct width
-        width_entry = Entry(input_frame, textvariable=self.flow_rate_var, width=7)  # auto updates when user types in field
+        self.flow_rate_entry = Entry(input_frame, textvariable=self.flow_rate_var, width=7)  # auto updates when user types in field
         # positions entry widget
-        width_entry.grid(row=2, column=1, sticky="w", pady=5)
-        Label(input_frame, text="(L/s)", bg="white").grid(row=2, column=2, sticky="e", pady=5)  # Unit label
+        self.flow_rate_entry.grid(row=2, column=1, sticky="w", pady=5)
+        self.flow_rate_unit = Label(input_frame, text="(L/s)", bg="white")  # unit instance
+        self.flow_rate_unit.grid(row=2, column=2, sticky="e", pady=5)  # grid position of unit instance
 
         # BUTTONS -- input by clicking
         # CALCULATE
         # Create the Calculate/Run button
-        calculate_button = Button(input_frame,  # our button's frame
+        self.calculate_button = Button(input_frame,  # our button's frame
                                   text="Calculate",  # text in button
                                   command=self.run_calculation,  # this calls run calc
                                   padx=10,  # button x & y padding
                                   pady=5    
                                   )
         # grid -- apply button to our field gird position
-        calculate_button.grid(row=3, column=0, columnspan=2, pady=10, sticky="w")
+        self.calculate_button.grid(row=3, column=0, columnspan=2, pady=10, sticky="w")
+
+
+    # hides and renames fields to appropriate duct type
+    # this is linked to our duct_type dropdown
+    def toggle_duct_type_fields(self, *args):
+        # *args = we unpack all the instance's args instead of trying to add all back in
+        # first we get the duct type
+        duct_type = self.duct_type_var.get()  # use .get() to "get" it
+
+        # now we hide/add fields based on selected duct type in dropdown
+        match (duct_type):  # duct_type is our match case input
+            case "Rectangular":  # for a rect duct
+                # set width label to duct width using config()
+                self.width_label.config(text="Duct Width")
+
+                # show the height field
+                self.height_label.grid(row=1, column=3, sticky="w", pady=5)
+                self.height_entry.grid(row=1, column=4, sticky="w", pady=5)
+                self.height_unit.grid(row=1, column=5, sticky="e", pady=5)
+
+            case "Round":  # for a round duct
+                # set width label to duct width using config()
+                self.width_label.config(text="Duct Diameter")
+
+                # HIDE the height field for round ducts using .grid_remove()
+                self.height_label.grid_remove()
+                self.height_entry.grid_remove()
+                self.height_unit.grid_remove()
+
 
     # this method is what's called when clicking the calculate/run button
     def run_calculation(self):
@@ -147,12 +190,23 @@ class UI:
         # use a try-except block in case an input is missing
         try:
             # all fields are strs, need to convert to correct types
-            width = int(self.width_var.get())  # duct width
-            height = int(self.height_var.get())  # duct height
-            diameter = width  # DIAMETER (TODO: Placeholder for when implementing Roudn calcs)
+            # common fields to ALL duct types
             flow_rate = int(self.flow_rate_var.get())  # duct flow rate
 
-            # call the controller to provide us with the calculated results
+            # now let's only valid inputs that are required for each duct type
+            # no height for round etc.
+            match (duct_type):  # duct_type is our match case input
+                case "Rectangular":  # rect duct type
+                    width = int(self.width_var.get())  # duct width
+                    height = int(self.height_var.get())  # duct height
+                    diameter = None  # not used
+                case "Round":  # round duct type
+                    diameter = int(self.width_var.get())  # duct diameter
+                    width = diameter  # Not really used, but keep for API compatibility
+                    height = None  # not used
+
+            # now that we've validated the correct duct types, let's process the results
+            # call the controller to provide us with the calculated results (from duct.py)
             results = self.controller.duct_properties(duct_type, width, height, diameter, flow_rate)
             
             # now that we have the inputs & calculations successfully, let's display the results!
